09b151a9e4f600c309555d34f648998d4ef72f2e
firatcanulukaya <mua.firat@gmail.com>
2022-04-14 21:49:59 +0300

    authCheck hatası giderildi

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const authCheck = require("./middlewares/authCheck.middleware");
 const studentRoute = require("./routes/student.route");
 const authRoute = require("./routes/auth.route");
 
-app.use("/api/student", authCheck(), studentRoute);
+app.use("/api/student", authCheck, studentRoute);
 app.use("/api/auth", authRoute);
 
 app.listen(3001, () => console.log(`Server started at http://localhost:3001`))
