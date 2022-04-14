eaef14db4d83af1b1d377ede420ece158a026cb8
firatcanulukaya <mua.firat@gmail.com>
2022-04-14 21:37:11 +0300

    authCheck eklendi

--- server/index.js
+++ server/index.js
@@ -6,10 +6,12 @@ const app = express();
 app.use(json())
 app.use(cors())
 
+const authCheck = require("./middlewares/authCheck.middleware");
+
 const studentRoute = require("./routes/student.route");
 const authRoute = require("./routes/auth.route");
 
-app.use("/api/student", studentRoute);
+app.use("/api/student", authCheck(), studentRoute);
 app.use("/api/auth", authRoute);
 
 app.listen(3001, () => console.log(`Server started at http://localhost:3001`))
