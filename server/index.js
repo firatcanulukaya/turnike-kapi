833beb6beed7bc6af57a8741f1438d818e768c0a
firatcanulukaya <mua.firat@gmail.com>
2022-04-14 21:18:42 +0300

    giriş kontrolü kaldırıldı

--- server/index.js
+++ server/index.js
@@ -8,9 +8,8 @@ app.use(cors())
 
 const studentRoute = require("./routes/student.route");
 const authRoute = require("./routes/auth.route");
-const authCheck = require("./middlewares/authCheck.middleware");
 
-app.use("/api/student", authCheck, studentRoute);
+app.use("/api/student", studentRoute);
 app.use("/api/auth", authRoute);
 
 app.listen(3001, () => console.log(`Server started at http://localhost:3001`))
