9fabdf2b4e2b7fd49837ecbba01d55bde6b9c8d7
firatcanulukaya <mua.firat@gmail.com>
2022-04-14 18:02:21 +0300

    route'lar için yetki kontrol ve giriş kontrol'ü eklendi

--- server/routes/student.route.js
+++ server/routes/student.route.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const {createStudent, getAllStudents, getStudentById, updateStudent, deleteStudent, deleteAllStudents} = require("../controllers/student.controller");
+const {
+    createStudent,
+    getAllStudents,
+    getStudentById,
+    updateStudent,
+    deleteStudent,
+    deleteAllStudents
+} = require("../controllers/student.controller");
+const roleCheck = require("../middlewares/roleCheck.middleware");
+const authCheck = require("../middlewares/authCheck.middleware");
 
-router.post("/create", createStudent);
-router.get("/getAll", getAllStudents);
-router.get("/get/:id", getStudentById);
-router.patch("/update/:id", updateStudent);
-router.delete("/delete/:id", deleteStudent);
-router.delete("/deleteAll", deleteAllStudents);
+router.post("/create", roleCheck([2]), authCheck(), createStudent);
+router.get("/getAll", authCheck(), getAllStudents);
+router.get("/get/:id", authCheck(), getStudentById);
+router.delete("/delete/:id", roleCheck([2]), authCheck(), deleteStudent);
+router.delete("/deleteAll", roleCheck([2]), authCheck(), deleteAllStudents);
 
 module.exports = router;
