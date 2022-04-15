e80c87cdb743014b6065342fc57956d6992c031d
firatcanulukaya <mua.firat@gmail.com>
2022-04-15 15:00:09 +0300

    crud işlemleri

--- server/controllers/covidTest.controller.js
+++ server/controllers/covidTest.controller.js
@@ -46,6 +46,160 @@ const createResult = async (req, res) => {
     }
 };
 
+const getResults = async (req, res) => {
+    try {
+        const covidTests = await db.CovidTest.findAll({
+            where: {}
+        });
+
+        if (covidTests.length === 0) {
+            throw new Error("NO_RESULTS_FOUND");
+        }
+
+        MessageService(res, covidTests);
+    } catch (error) {
+        if (error.message.includes("NO_RESULTS_FOUND")) {
+            ErrorService(res, {message: "NO_RESULTS_FOUND"});
+        } else {
+            ErrorService(res, error);
+        }
+    }
+};
+
+const getResult = async (req, res) => {
+    try {
+        const {barcode} = req.params;
+        const covidTest = await db.CovidTest.findOne({
+            where: {
+                barcode
+            }
+        });
+
+        if (!covidTest) {
+            throw new Error("RESULT_NOT_FOUND");
+        }
+
+        MessageService(res, covidTest);
+    } catch (error) {
+        if (error.message.includes("RESULT_NOT_FOUND")) {
+            ErrorService(res, {message: "RESULT_NOT_FOUND"});
+        } else {
+            ErrorService(res, error);
+        }
+    }
+};
+
+const getResultById = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const covidTest = await db.CovidTest.findOne({
+            where: {
+                id
+            }
+        });
+
+        if (!covidTest) {
+            throw new Error("RESULT_NOT_FOUND");
+        }
+
+        MessageService(res, covidTest);
+    } catch (error) {
+        if (error.message.includes("RESULT_NOT_FOUND")) {
+            ErrorService(res, {message: "RESULT_NOT_FOUND"});
+        } else {
+            ErrorService(res, error);
+        }
+    }
+};
+
+const updateResult = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {idCard, barcode, testResult, testDate} = req.body;
+
+        const covidTest = await db.CovidTest.findOne({
+            where: {
+                id
+            }
+        });
+
+        if (!covidTest) {
+            throw new Error("RESULT_NOT_FOUND");
+        }
+
+        const updatedCovidTest = await covidTest.update({
+            idCard,
+            barcode,
+            testResult,
+            testDate
+        });
+
+        MessageService(res, updatedCovidTest);
+    } catch (error) {
+        if (error.message.includes("RESULT_NOT_FOUND")) {
+            ErrorService(res, {message: "RESULT_NOT_FOUND"});
+        } else {
+            ErrorService(res, error);
+        }
+    }
+};
+
+
+const deleteResult = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const covidTest = await db.CovidTest.destroy({
+            where: {
+                id
+            }
+        });
+
+        if (!covidTest) {
+            throw new Error("RESULT_NOT_FOUND");
+        }
+
+        MessageService(res, "ok");
+    } catch (error) {
+        if (error.message.includes("RESULT_NOT_FOUND")) {
+            ErrorService(res, {message: "RESULT_NOT_FOUND"});
+        } else {
+            ErrorService(res, error);
+        }
+    }
+};
+
+const deleteAllResults = async (req, res) => {
+    try {
+
+        const covidTests = await db.CovidTest.findAll({
+            where: {}
+        });
+
+        if (covidTests.length === 0) {
+            throw new Error("NO_RESULTS_FOUND");
+        }
+
+        await db.CovidTest.destroy({
+            where: {},
+            truncate: true
+        });
+
+        MessageService(res, "ok");
+    } catch (error) {
+        if (error.message.includes("NO_RESULTS_FOUND")) {
+            ErrorService(res, {message: "NO_RESULTS_FOUND"});
+        } else {
+            ErrorService(res, error);
+        }
+    }
+};
+
 module.exports = {
-    createResult
+    createResult,
+    getResults,
+    getResult,
+    getResultById,
+    updateResult,
+    deleteResult,
+    deleteAllResults
 };
--- server/routes/covidTest.route.js
+++ server/routes/covidTest.route.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-const {createResult} = require("../controllers/covidTest.controller");
+const {
+    createResult,
+    getResults,
+    getResult,
+    updateResult,
+    deleteAllResults,
+    deleteResult,
+    getResultById
+} = require("../controllers/covidTest.controller");
 
 router.post("/create", createResult);
+router.get("/getAll", getResults);
+router.get("/getByBarcode/:barcode", getResult);
+router.get("/getById/:id", getResultById);
+router.patch("/update/:id", updateResult);
+router.delete("/deleteAll", deleteAllResults);
+router.delete("/delete/:id", deleteResult);
 
 module.exports = router;
