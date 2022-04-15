61d578482b14aba5f011132e336dbe17b3c4c188
firatcanulukaya <mua.firat@gmail.com>
2022-04-15 23:02:39 +0300

    rfid route'u eklendi

--- server/index.js
+++ server/index.js
@@ -11,9 +11,11 @@ const authCheck = require("./middlewares/authCheck.middleware");
 const studentRoute = require("./routes/student.route");
 const authRoute = require("./routes/auth.route");
 const covidTestRoute = require("./routes/covidTest.route");
+const rfidRoute = require("./routes/rfidPass.route");
 
 app.use("/api/student", authCheck, studentRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/covid", authCheck, covidTestRoute);
+app.use("/api/pass", rfidRoute);
 
 app.listen(3001, () => console.log(`Server started at http://localhost:3001`))
