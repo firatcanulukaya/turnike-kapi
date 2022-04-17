00e946407e1e0a937788ec9d424ef128b99698c9
firatcanulukaya <mua.firat@gmail.com>
2022-04-17 14:33:44 +0300

    kapı açılma işlemleri için gerekli kontroller

--- server/controllers/rfidPass.controller.js
+++ server/controllers/rfidPass.controller.js
@@ -1,6 +1,7 @@
 const db = require('../models');
 const ErrorService = require("../services/error.service");
 const MessageService = require("../services/message.service");
+const moment = require('moment');
 
 const rfidPass = async (req, res) => {
     try {
@@ -8,17 +9,53 @@ const rfidPass = async (req, res) => {
         const student = await db.Student.findOne({
             where: {
                 rfid
-            }
+            },
+            attributes: {
+                exclude: ["password"]
+            },
+            include: [{
+                model: db.CovidTest,
+                as: "covidTest",
+                attributes: ["testDate"],
+            }]
         });
 
         if (!student) {
             throw new Error("STUDENT_NOT_FOUND");
         }
 
-        MessageService(res, true);
+        const covid = await db.CovidTest.findOne({
+            where: {
+                userId: student.id
+            },
+            attributes: ["testDate"],
+        });
+
+        if (!covid) {
+            throw new Error("COVID_TEST_NOT_FOUND");
+        }
+
+        const jsonedCovid = covid.toJSON();
+        var parts = jsonedCovid.testDate.split('/');
+
+        const currentDate = new Date();
+        const testDate = new Date(parts[2], parts[1] - 1, parts[0]);
+
+        var a = moment([currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate()]);
+        var b = moment([testDate.getFullYear(), testDate.getMonth(), testDate.getDate()]);
+        var diff = a.diff(b, 'days');
+
+        if (diff >= 7) {
+            MessageService(res, false);
+        } else {
+            MessageService(res, true);
+        }
+
     } catch (error) {
         if (error.message.includes("STUDENT_NOT_FOUND")) {
             ErrorService(res, {message: "STUDENT_NOT_FOUND"});
+        } else if (error.message.includes("COVID_TEST_NOT_FOUND")) {
+            ErrorService(res, {message: "COVID_TEST_NOT_FOUND"});
         } else {
             ErrorService(res, error);
         }
