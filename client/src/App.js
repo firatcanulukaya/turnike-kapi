6e6c6d332042e0cf3ee80b1bc423d768e93d5ec7
firatcanulukaya <mua.firat@gmail.com>
2022-04-20 14:05:08 +0300

    route altyapısı

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import {useSelector} from "react-redux";
+import {Routes, Route} from "react-router-dom";
+import {routes} from "./routes";
+import "./assets/style/style.css";
 
 const App = () => {
     const {test} = useSelector(state => state.utils);
     return (
         <div>
-            {test}
+            <Routes>
+                {routes.map((route, index) => (
+                    <Route key={index} exact={route.exact} path={route.path}
+                           element={<route.component/>}/>
+                ))}
+            </Routes>
         </div>
     );
 }
---
+++ client/src/routes/index.js
@@ -0,0 +1,9 @@
+import Login from "../components/Login/Login";
+
+export const routes = [
+    {
+        path: '/',
+        component: Login,
+        exact: true,
+    }
+]
