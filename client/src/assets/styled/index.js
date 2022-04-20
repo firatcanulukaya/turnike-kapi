7ebb8825ad1781b0e08191966a252adbc7502b20
firatcanulukaya <mua.firat@gmail.com>
2022-04-20 14:17:09 +0300

    genel styled düzenlendi

--- client/src/assets/styled/index.js
+++ client/src/assets/styled/index.js
@@ -4,22 +4,29 @@ export const Container = styled.div`
   width: 100%;
   margin-right: auto;
   margin-left: auto;
+  padding: 15px;
 `
 
 export const Row = styled.div`
   display: flex;
   flex-wrap: wrap;
+  margin-right: -15px;
+  margin-left: -15px;
 `
 
 export const ColFull = styled.div`
   width: 100%;
   max-width: 100%;
+  ${props => props.center && `
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `};
 `
 
 export const ColFifty = styled.div`
   width: 100%;
   max-width: 50%;
-  min-height: 100vh;
   ${props => props.center && `
     display: flex;
     align-items: center;
@@ -36,6 +43,5 @@ export const ColFifty = styled.div`
   @media only screen and (min-width: 1200px) {
     width: 100%;
     max-width: 50%;
-    height: 100vh;
   }
 `
