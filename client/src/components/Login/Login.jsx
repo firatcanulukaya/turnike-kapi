f19c83e135bdb2b29ee2c3d1c377bfb8030234b1
firatcanulukaya <mua.firat@gmail.com>
2022-04-21 10:21:49 +0300

    giriş yapma sayfası oluşturuldu

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,8 +1,121 @@
+import {loginUser} from "../../redux/actions/userAction";
+import {useDispatch, useSelector} from "react-redux";
+import {
+    Button,
+    ColFull,
+    Container,
+    Divider,
+    FormError,
+    Input, Label,
+    Row,
+    SubTitle,
+    Title
+} from "../../assets/styled";
+import {LoginContainer, LoginLogo, LoginTitle} from "./style";
+import {useForm} from "react-hook-form";
+import logo from "../../assets/img/oomlLogo.png";
+import {useEffect} from "react";
+
 const Login = () => {
-    return(
-        <>
-            asds
-        </>
+    const dispatch = useDispatch();
+    const {user} = useSelector(state => state.user);
+
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm();
+
+    useEffect(() => {
+        console.log(user);
+    }, [user]);
+
+    const onSubmit = data => {
+        dispatch(loginUser(data));
+    };
+
+    return (
+        <Container>
+            <Row>
+                <ColFull center>
+                    <LoginLogo src={logo}/>
+                </ColFull>
+            </Row>
+            <Row>
+                <ColFull center>
+                    <Title>
+                        Osman Örek Meslek Lisesi Kapısına Hoşgeldiniz.
+                    </Title>
+                </ColFull>
+            </Row>
+
+            <Row style={{marginTop: "2rem"}}>
+                <ColFull center>
+                    <LoginTitle>
+                        Giriş Yap
+                    </LoginTitle>
+                </ColFull>
+                <ColFull center>
+                    <SubTitle>
+                        İşleme devam edebilmek için lütfen giriş yapınız.
+                    </SubTitle>
+                </ColFull>
+
+                <ColFull center>
+                    <LoginContainer>
+                        <form onSubmit={handleSubmit(onSubmit)} style={{width: "100%"}}>
+                            <Label>
+                                <Input placeholder="Kimlik Numaranızı giriniz" type="number"
+                                       className={errors.idCard ? "error" : ""}
+                                       {...register("idCard", {
+                                           required: "Lütfen gerekli yerleri doldurunuz.",
+                                           minLength: {
+                                               value: 6,
+                                               message: "Lütfen en az 6 karakter giriniz.",
+                                           },
+                                           maxLength: {
+                                               value: 11,
+                                               message: "Lütfen en fazla 11 karakter giriniz."
+                                           }
+                                       })}/>
+                                {errors.idCard && (
+                                    <FormError>
+                                        <p><i className="fa-solid fa-circle-exclamation"/> {errors.idCard?.message}</p>
+                                    </FormError>
+                                )}
+                            </Label>
+
+                            <Label>
+                                <Input placeholder="Şifrenizi giriniz"
+                                       className={errors.password ? "error" : ""}
+                                       {...register("password", {
+                                           required: "Lütfen gerekli yerleri doldurunuz.",
+                                           minLength: {
+                                               value: 4,
+                                               message: "Lütfen en az 8 karakter giriniz.",
+                                           },
+                                           maxLength: {
+                                               value: 32,
+                                               message: "Lütfen en fazla 32 karakter giriniz.",
+                                           },
+                                       })}
+                                />
+
+                                {errors.password && (
+                                    <FormError>
+                                        <p><i className="fa-solid fa-circle-exclamation"/> {errors.password?.message}
+                                        </p>
+                                    </FormError>
+                                )}
+                            </Label>
+                            <Button>Giriş Yap</Button>
+                        </form>
+                        <Divider/>
+                    </LoginContainer>
+                </ColFull>
+            </Row>
+
+        </Container>
     )
 }
 
--- client/src/components/Login/style.js
+++ client/src/components/Login/style.js
@@ -1,1 +1,20 @@
 import styled from 'styled-components';
+
+export const LoginLogo = styled.img`
+  width: 100px;
+  margin-bottom: 20px;
+`;
+
+export const LoginTitle = styled.h1`
+  font-size: 24px;
+  font-weight: 500;
+  margin-bottom: 20px;
+`;
+
+export const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 50%;
+`;
