4ed2b7e85e383294901ebb50e9d9f50b6620f0f0
firatcanulukaya <mua.firat@gmail.com>
2022-04-21 12:09:56 +0300

    covidtest route'u eklendi

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,9 +1,15 @@
 import Login from "../components/Login/Login";
+import CovidTest from "../components/CovidTest/CovidTest";
 
 export const routes = [
     {
         path: '/',
         component: Login,
+        exact: true,
+    },
+    {
+        path: '/test-yukle',
+        component: CovidTest,
         exact: true,
     }
 ]
