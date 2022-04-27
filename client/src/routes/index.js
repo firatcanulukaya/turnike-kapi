c25ae2b94919481a8e5fabcd1c2537e1a29ac792
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-04-27 11:12:32 +0300

    kayit route

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,11 +1,17 @@
 import Login from "../components/Login/Login";
 import CovidTest from "../components/CovidTest/CovidTest";
 import StudentPanel from "../components/StudentPanel/StudentPanel";
+import Register from "../components/Register/Register";
 
 export const routes = [
     {
         path: '/',
         component: Login,
+        exact: true,
+    },
+    {
+        path: '/kayit',
+        component: Register,
         exact: true,
     },
     {
