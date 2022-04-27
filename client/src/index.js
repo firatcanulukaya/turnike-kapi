da53422544cfd65587e11655bccbd493797c9372
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-04-27 10:43:05 +0300

    chakra

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,16 @@ import App from './App';
 import store from './redux';
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
+import {ChakraProvider} from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
             <BrowserRouter>
-                <App/>
+                <ChakraProvider>
+                    <App/>
+                </ChakraProvider>
             </BrowserRouter>
         </Provider>
     </React.StrictMode>
