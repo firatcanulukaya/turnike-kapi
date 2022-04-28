cc940a59a56a6ba4fa0e7b15913676523f5e1a7c
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-04-28 15:37:28 +0300

    yeni rota

--- server/routes/rfidPass.route.js
+++ server/routes/rfidPass.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const {rfidPass} = require("../controllers/rfidPass.controller");
+const {rfidPass, getOut} = require("../controllers/rfidPass.controller");
 
-router.get("/:rfid", rfidPass);
+router.get("/in/:rfid", rfidPass);
+router.get("/out/:rfid", getOut);
 
 module.exports = router;
